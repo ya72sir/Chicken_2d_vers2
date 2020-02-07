4de84b065535b1c5c19b666ab9c3ac321902edeb
ya72sir <ya72sir@yandex.ru>
2020-02-07 20:23:25 +0300

    Add cpp code

---
+++ Source/Chicken2D/CppMainPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "CppMainPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHICKEN2D_API ACppMainPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
